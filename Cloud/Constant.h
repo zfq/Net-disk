d57a7530ac26ad41653a8cad4317025c2545ff1e
zfq <1586687169@qq.com>
2014-02-09 22:30:12 +0800

    添加文件

---
+++ Cloud/Constant.h
@@ -0,0 +1,29 @@
+//
+//  Constant.h
+//  Cloud
+//
+//  Created by zhaofuqiang on 13-12-28.
+//  Copyright (c) 2013年 zzti. All rights reserved.
+//
+
+#ifndef Cloud_Constant_h
+#define Cloud_Constant_h
+
+#define ServiceName @"com.ccm.Cloud"
+#define AccountName @"AccountName"
+
+#define HOST_URL        @"http://202.85.212.220:8080/paas/"
+#define ROOT_PATH       @"/"
+#define MAX_SHOW_NUM    100
+
+#define kLoginStatusChangedNotification @"kLoginStatusChangedNotification"
+#define NavigationBarColor [UIColor colorWithRed:0.118 green:0.564 blue:0.95 alpha:1.0]
+
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#define X_OFFSET 81.0
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 70000
+#define IOS7_SDK_AVAILABLE 1
+#endif
+
+#endif
