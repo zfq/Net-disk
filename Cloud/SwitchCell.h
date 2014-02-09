17054da7aaee258ddc4b548faefb6106e10d92fe
zfq <1586687169@qq.com>
2014-02-09 22:24:23 +0800

    添加文件

---
+++ Cloud/SwitchCell.h
@@ -0,0 +1,15 @@
+//
+//  SwitchCell.h
+//  Cloud
+//
+//  Created by zhaofuqiang on 13-12-27.
+//  Copyright (c) 2013年 zzti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SwitchCell : UITableViewCell
+
+@property (nonatomic,weak) IBOutlet UILabel *label;
+@property (nonatomic,weak) IBOutlet UISwitch *toggle;
+@end
---
+++ Cloud/SwitchCell.m
@@ -0,0 +1,20 @@
+//
+//  SwitchCell.m
+//  Cloud
+//
+//  Created by zhaofuqiang on 13-12-27.
+//  Copyright (c) 2013年 zzti. All rights reserved.
+//
+
+#import "SwitchCell.h"
+
+@implementation SwitchCell
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
