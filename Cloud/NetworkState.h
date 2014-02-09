3b57911e4fa7cc0d533d59b8db02a9b157ce1cbd
zfq <1586687169@qq.com>
2014-02-09 22:23:48 +0800

    添加文件

---
+++ Cloud/NetworkState.h
@@ -0,0 +1,14 @@
+//
+//  NetworkState.h
+//  Cloud
+//
+//  Created by zzti on 13-12-23.
+//  Copyright (c) 2013年 zzti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetworkState : NSObject
+
++ (BOOL)networkState;
+@end
