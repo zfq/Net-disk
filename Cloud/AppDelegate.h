29a899093471f56d161c79c35880b8fb104601ba
zfq <1586687169@qq.com>
2014-02-09 17:04:19 +0800

    Initial Commit

---
+++ Cloud/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Cloud
+//
+//  Created by zhaofuqiang on 14-2-9.
+//  Copyright (c) 2014年 zhaofuqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Cloud/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cloud
+//
+//  Created by zhaofuqiang on 14-2-9.
+//  Copyright (c) 2014年 zhaofuqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
